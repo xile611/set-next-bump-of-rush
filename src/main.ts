5b40139566aa200299efeea6eab441f00e1aebb3
kangxiaoting.kk <kangxiaoting.kk@bytedance.com>
2023-09-26 11:18:34 +0800

    feat: add check of versions

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,28 @@
 import * as core from '@actions/core'
 import {
+  getCurrentVersionOfMainPackage,
   parseNextBumpFromVersion,
   readBumpType,
   writeNextBump
 } from './read-policies'
+import {gt} from 'semver'
 
 async function run(): Promise<void> {
   try {
     const rushPath = core.getInput('rush_path')
     const policyName = core.getInput('policy_name')
     const releaseVersion = core.getInput('release_version')
-    const isPrerelease = core.getInput('is_prerelease')
+    const isPrerelease = core.getInput('is_prerelease') === 'true'
     const needWrite = core.getInput('write_next_bump')
 
-    const parsedBump =
-      isPrerelease === 'true'
-        ? 'prerelease'
-        : releaseVersion
-        ? parseNextBumpFromVersion(releaseVersion)
-        : undefined
+    const parsedBump = isPrerelease
+      ? 'prerelease'
+      : releaseVersion
+      ? parseNextBumpFromVersion(releaseVersion)
+      : undefined
 
     if (!parsedBump) {
-      if (isPrerelease !== 'true' && !releaseVersion) {
+      if (isPrerelease && !releaseVersion) {
         core.warning(
           `[warn] you should add parameter 'release_version' when 'is_prerelease' is false or empty`
         )
@@ -34,6 +35,24 @@ async function run(): Promise<void> {
       core.info(
         `[info] parse nextBump(${parsedBump}) from release_verison: ${releaseVersion}`
       )
+    }
+
+    if (releaseVersion) {
+      const versions = getCurrentVersionOfMainPackage(rushPath, policyName)
+
+      if (versions?.version && !gt(releaseVersion, versions.version)) {
+        core.setFailed(
+          `[error] the release version ${releaseVersion} is not greater than version(${versions.version}) in version-policy`
+        )
+        return
+      }
+
+      if (versions?.mainVersion && !gt(releaseVersion, versions.mainVersion)) {
+        core.setFailed(
+          `[error] the release version ${releaseVersion} is not greater than version(${versions.mainVersion}) in package.json`
+        )
+        return
+      }
     }
 
     if (needWrite === 'true' && parsedBump) {
